b22bb9c67143120fad6938d66b541c3babb008a3
Victor Hugo <victormotabatista@gmail.com>
2022-10-24 22:18:14 -0300

    chore():implementing the challenge

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -5,22 +5,37 @@ namespace DesafioPOO.Models
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
-        {
-            Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
-        }
+        public string Modelo { get; set; }
 
-        public void Ligar()
-        {
-            Console.WriteLine("Ligando...");
-        }
+        public string IMEI { get; set; }
 
-        public void ReceberLigacao()
-        {
-            Console.WriteLine("Recebendo ligação...");
-        }
+        public int Memoria { get; set; }
 
-        public abstract void InstalarAplicativo(string nomeApp);
+
+    public Smartphone(string numero, string modelo, string imei, int memoria)
+    {
+        Numero = numero;
+        // TODO: Passar os parâmetros do construtor para as propriedades
+        Modelo = modelo;
+        IMEI = imei;
+        Memoria = memoria;
+    }
+
+    protected Smartphone(string numero)
+    {
+        Numero = numero;
+    }
+
+    public void Ligar()
+    {
+        Console.WriteLine("Ligando...");
+    }
+
+    public void ReceberLigacao()
+    {
+        Console.WriteLine("Recebendo ligação...");
+    }
+
+    public abstract void InstalarAplicativo(string nomeApp);
     }
 }
